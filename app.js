2826666e7be65a06389c2fb6c4d7552bb9a5a457
x_XEOJ_x <arae468468@gmail.com>
2022-01-27 14:34:24 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+let typeString = ["Welcome to Clearnote !"];
+let i = 0;
+let count =0;
+let SelectedText = '';
+let text ='';
+
+(function type(){
+    if(count == typeString.length){
+        count = 0;
+    }
+    SelectedText = typeString[count];
+    text = SelectedText.slice(0,++i);
+    document.getElementById("typing").innerHTML = text;
+    /*if(text.length === SelectedText.length){
+        count++;
+        i=0;
+    }*/
+    setTimeout(type,190);
+
+}());
+
