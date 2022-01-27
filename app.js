9ad5fbadd9071d162462062752423778da9deef4
x_XEOJ_x <arae468468@gmail.com>
2022-01-27 16:53:58 +0700

    add nav

--- app.js
+++ app.js
@@ -18,5 +18,15 @@ let text ='';
     setTimeout(type,190);
 
 }());
+
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-nav-toggle');
+
+navBarToggle.addEventListener("click",function(){
+    mainNav.classList.toggle('active')
+})
+
+
+
 /*https://unsplash.com/s/photos/note*/
 
